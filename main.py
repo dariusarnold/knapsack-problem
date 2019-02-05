2693d0f95305908aa27512f2f2e46646a8891009
Darius Arnold <mail@dariusarnold.de>
2019-02-05 21:19:22 +0100

    Filter out combinations above weight limit to save processing

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 
 import csv
 import argparse
-import pprint
 
 
 class Item:
@@ -70,7 +69,7 @@ def read_file(filename):
     return items
 
 
-def merge(old_list, new_list):
+def find(list_, criterion, default):
     """
     Return the index of the first item that satisfies the criterion in list_. If no item satisfies the criterion,
     the default value is returned
@@ -79,42 +78,48 @@ def merge(old_list, new_list):
     :param default: Default value to return if no item matches criterion
     :return: Index of first item for which criterion returns true
     """
+    return next((index for index, el in enumerate(list_) if criterion(el)), default)
 
-    def find_larger(list_, value, default=None):
+
+def find_larger_profit(list_, value, default=None):
     """
     Return the index of the first item in list__ whose profit is larger than value
     """
+    return find(list_, lambda x: x.profit > value, default)
 
-        def find(list_, criterion, default):
+
+def find_max_below(list_, value, default=None):
     """
     Return the index of the item in the list whose weight is closest to, but smaller than value
     """
     return find(list_, lambda x: x.weight > value, default) - 1
 
-            return next((index for index, el in enumerate(list_) if criterion(el)), default)
 
-        return find(list_, lambda x: x.profit > value, default)
+def merge(old_list, new_list, weight_limit):
     """
     Takes two lists of pareto-optimal points and merges them, discarding points that are dominated by others.
     Point A is dominated by point B if B achieves a larger profit with the same or less weight than A.
     :param old_list: Previous list of pareto optimal points
     :param new_list: New List of pareto optimal points, where one item was added to every point from the old list
+    :param weight_limit: Max weight to reduce number of points
     :return: Merged list excluding points that are dominated by others
     """
 
     merged_list = []
     profit_max = -1
     while True:
-        old_pi = find_larger(old_list, profit_max)
-        new_pi = find_larger(new_list, profit_max)
-        if old_pi is None:
-            merged_list += new_list[new_pi:]
+        old_point_index = find_larger_profit(old_list, profit_max)
+        new_point_index = find_larger_profit(new_list, profit_max)
+        # merge other list if one doesn't contain a point with profit above the current limit
+        # or the found point is above the weight limit
+        if old_point_index is None or old_list[old_point_index].weight > weight_limit:
+            merged_list += new_list[new_point_index:]
             break
-        if new_pi is None:
-            merged_list += old_list[old_pi:]
+        if new_point_index is None or new_list[new_point_index].weight > weight_limit:
+            merged_list += old_list[old_point_index:]
             break
-        old_p = old_list[old_pi]
-        new_p = new_list[new_pi]
+        old_p = old_list[old_point_index]
+        new_p = new_list[new_point_index]
         if old_p.weight < new_p.weight or (old_p.weight == new_p.weight and old_p.profit > new_p.profit):
             merged_list.append(old_p)
             profit_max = old_p.profit
@@ -136,8 +141,11 @@ def knapsack(items, weight_limit):
     # add items to possible pareto optimal sets one by one, but merge by only keeping the optimal points
     for item in items:
         # filter out item combinations that lie above the weight limit
-        content = merge(content, content_next)
-    pprint.pprint(content)
+        content_next = [point + item for point in content]
+        content = merge(content, content_next, weight_limit)
+    best_combination_below_limit = content[find_max_below(content, weight_limit)]
+    print(f"Best set: {best_combination_below_limit}")
+    return best_combination_below_limit.item_ids
 
 
 def main():
@@ -148,6 +156,6 @@ def main():
     items = read_file(args.csv_filename)
     knapsack(items, args.weight_limit)
 
+
 if __name__ == '__main__':
     main()
-
